61683c3743e72e8a069e066957aa6ad7754c4004
joscary <joscary__vanessa@hotmail.com>
2024-02-18 21:13:09 -0300

    favicon en cada html y creando contacto con un icono carrito de compras

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,25 @@
 // menu hamburguesa //
-const navbarcustom1 = document.querySelector("#navbarcustom1");
-const abrirmenu = document.querySelector("#abrirmenu");
-const cerrarmenu = document.querySelector("#cerrarmenu");
+function inicializarMenuHamburguesa() {
+  const navbarcustom1 = document.querySelector("#navbarcustom1");
+  const abrirmenu = document.querySelector("#abrirmenu");
+  const cerrarmenu = document.querySelector("#cerrarmenu");
 
-abrirmenu.addEventListener("click", () => {
-  navbarcustom1.classList.add("visible");
-  abrirmenu.style.display = "none";
-  cerrarmenu.style.display = "block";
-});
-cerrarmenu.addEventListener("click", () => {
-  navbarcustom1.classList.remove("visible");
-  abrirmenu.style.display = "block";
-  cerrarmenu.style.display = "none";
-});
+  abrirmenu.addEventListener("click", () => {
+    navbarcustom1.classList.add("visible");
+    abrirmenu.style.display = "none";
+    cerrarmenu.style.display = "block";
+  });
 
- // STARS //
+  cerrarmenu.addEventListener("click", () => {
+    navbarcustom1.classList.remove("visible");
+    abrirmenu.style.display = "block";
+    cerrarmenu.style.display = "none";
+  });
+}
+inicializarMenuHamburguesa();
+
+// STARS //
+
 function rateStar(star) {
   const ratingContainer = star.parentNode;
   const stars = ratingContainer.getElementsByClassName("star");
@@ -31,4 +36,23 @@ function rateStar(star) {
   }
 }
 
+//SHOPPING CARD
 
+document.addEventListener("DOMContentLoaded", function () {
+  var numeroArticulosElement = document.getElementById("numeroArticulos");
+  var cantidadArticulos = 0;
+
+  function agregarAlCarrito() {
+    cantidadArticulos++;
+    actualizarNumeroArticulos();
+  }
+
+  function actualizarNumeroArticulos() {
+    numeroArticulosElement.textContent = cantidadArticulos;
+  }
+
+  var carritoIcono = document.querySelector(".carrito i");
+  carritoIcono.addEventListener("click", function () {
+    agregarAlCarrito();
+  });
+});
